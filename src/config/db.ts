c8662583f24418942d6a05627ea17a2a2c2db7f9
kamalesh <kamalesh@skyraan.com>
2026-06-22 17:00:44 +0530

    changes in swagger

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,15 @@ dotenv.config();
 
 export const db = mysql.createPool({
   host: "localhost",
-  user: process.env.USER || "root",
-  password: process.env.PASSWORD || "",
-  database: process.env.DB || "recipe_app",
+  user: "skyraantech_food_recipe",
+  password: "@eROf?=Nf!X?1W{y",
+  database: "skyraantech_food_recipe_db",
   connectionLimit: 10,
 });
+// export const db = mysql.createPool({
+//   host: "localhost",
+//   user: process.env.USER || "root",
+//   password: process.env.PASSWORD || "",
+//   database: process.env.DB || "recipe_app",
+//   connectionLimit: 10,
+// });
